808452ee1ee16beb10aaaed5c5196a4c6b476798
Klecianny Melo <kleciannymelo@gmail.com>
2023-08-03 21:30:26 -0300

    :sparkles: desenvolvendo o método post para createUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+// import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
-  @Get()
-  async getAllUsers() {
-    return JSON.stringify([{ name: 'Meg' }, { name: 'Ralf' }]);
+  /* Retorna o JSON de todos os usuários */
+  // @Get()
+  // async getAllUsers() {
+  //   return JSON.stringify([{ name: 'Meg' }, { name: 'Ralf' }]);
+  // }
+
+  @Post()
+  async createUser(@Body() createuser: CreateUserDto) {
+    return {
+      ...createuser,
+      password: 'Senha criptografada 🔐',
+    };
   }
 }
