fdf2d51f16d4673f44485a88e80af40ffdf0ce8b
Klecianny Melo <kleciannymelo@gmail.com>
2023-08-03 21:50:56 -0300

    :sparkles: conectando o controller ao createUser do service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 // import { Controller, Get } from '@nestjs/common';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
@@ -10,12 +11,20 @@ export class UserController {
   //   return JSON.stringify([{ name: 'Meg' }, { name: 'Ralf' }]);
   // }
 
+  constructor(private readonly userService: UserService) {}
+
+  // @Post()
+  // async createUser(@Body() createUser: CreateUserDto) {
+  //   /* Retorna todos os dados que o usuário enviou e no campo password retorna a string (independente do que o usuário tenha digitado) */
+  //   return {
+  //     ...createUser,
+  //     password: 'Senha criptografada 🔐',
+  //   };
+  // }
+
   @Post()
-  async createUser(@Body() createuser: CreateUserDto) {
-    /* Retorna todos os dados que o usuário enviou e no campo password retorna a string (independente do que o usuário tenha digitado) */
-    return {
-      ...createuser,
-      password: 'Senha criptografada 🔐',
-    };
+  async createUser(@Body() createUser: CreateUserDto) {
+    /* Conectando esse controller ao service de createUser */
+    return this.userService.createUser(createUser);
   }
 }
