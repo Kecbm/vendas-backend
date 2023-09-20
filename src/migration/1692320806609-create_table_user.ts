52bbe5eeb7e752d08cd25a396ba96b1bcf1333c0
Klecianny Melo <kleciannymelo@gmail.com>
2023-09-20 20:51:22 -0300

    :construction: alteração para o id ser autoincrementável

--- src/migration/1692320806609-create_table_user.ts
+++ src/migration/1692320806609-create_table_user.ts
@@ -3,9 +3,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CreateTableUser1692320806609 implements MigrationInterface {
   /* Aqui cria uma tabela no BD */
   public async up(queryRunner: QueryRunner): Promise<void> {
+    // id definido como SERIAL para gerar automaticamente os IDs
     await queryRunner.query(
       `CREATE TABLE "user" (
-        "id" integer NOT NULL,
+        "id" SERIAL,
         "name" character varying NOT NULL,
         "email" character varying NOT NULL,
         "cpf" character varying NOT NULL,
