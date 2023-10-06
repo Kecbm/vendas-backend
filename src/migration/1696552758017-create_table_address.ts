88713d68fb55ca31f964a44d1e136a3122cab266
Klecianny Melo <kleciannymelo@gmail.com>
2023-10-05 21:52:47 -0300

    :construction: parei no momento anterior a criação da tabela address

---
+++ src/migration/1696552758017-create_table_address.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+/* ESTOU EM: Seção 3 - aula 20 - 8:30 min */
+
+export class CreateTableAddress1696552758017 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+        `);
+  }
+}
