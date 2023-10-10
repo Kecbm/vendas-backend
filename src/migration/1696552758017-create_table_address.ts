07c421a6ad8d1e636bd6798a1fb8b456ebde2aa1
Klecianny Melo <kleciannymelo@gmail.com>
2023-10-09 21:13:59 -0300

    :sparkles: criando a tabela address

--- src/migration/1696552758017-create_table_address.ts
+++ src/migration/1696552758017-create_table_address.ts
@@ -1,15 +1,26 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-/* ESTOU EM: Seção 3 - aula 20 - 8:30 min */
-
 export class CreateTableAddress1696552758017 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.query(`
-        `);
+    queryRunner.query(
+      `CREATE TABLE "address" (
+        "id" SERIAL,
+        "user_id" integer NOT NULL,
+        "complement" character varying,
+        "number" integer NOT NULL,
+        "cep" character varying NOT NULL,
+        "city_id" integer NOT NULL,
+        "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        primary key ("id"),
+        foreign key ("user_id") references "user" ("id"),
+        foreign key ("city_id") references "city" ("id")
+      );
+      `,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.query(`
-        `);
+    await queryRunner.query(`DROP TABLE "address"`);
   }
 }
